faafa423306bcd4409fd838f3f16977ce2d8a7fc
marturiramya <2211cs010366@mallareddyuniversity.ac.in>
2025-01-24 11:17:57 +0530

    adding train txt files

---
+++ dom.js
@@ -0,0 +1,44 @@
+// !document.getElementById("");
+
+// 
+// let ele = document.getElementsByClassName("test");
+// // console.log(ele);
+// // console.log(Array.isArray(ele));
+// ele[0].Style.backgroundcolor="yellow"
+// //! spread operator :- it is used to takeout each and every value from the original array
+// //! syntax:- [...variable]
+// let x = [...ele];
+// console.log(x , Array.isArray(x));
+// x.map((element)=>{
+//     //console.log(element);
+//     element.style.backgroundcolor="teal";
+
+// // })
+// let ele = document.getElementsByTagName("div");
+// console.log(ele);
+
+
+// let ele = document.getElementById("div");
+// [...ele].map(element=>{
+//     element.style.background="aqua"
+
+//let ele= document.querySelector("#demo");
+//console.log(ele);
+
+//let ele = document.quarySelector(".test");
+//console.log(ele);
+// })
+// let ele= document.querySelectorAll(".test");
+// //console.log(ele);
+// [...ele].map(element=>{
+//     console.log(element.innerText);
+// }
+// )
+
+// ! events
+// let btn = document.getElementById("btn");
+// //btn.addEventListener("event", callbacks)
+// console.log("button clicked");
+// document.body.style.background="orange"
+
+let btn=  document.getElementById("btn");
